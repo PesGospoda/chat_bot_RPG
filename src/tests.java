63e9d96d0615ad5b0640837cdfc03b485a7286e8
Daniil <daniiltregubov@mail.ru>
2018-10-02 15:33:06 +0500

    new test

--- src/tests.java
+++ src/tests.java
@@ -1,6 +1,9 @@
 
 
 import org.junit.*;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 public class tests {
 
@@ -11,6 +14,15 @@ public class tests {
     	Player player = new Player();
     	player.getDamage(100);
         assertEquals(false, player.isAlive());
+    }
+    @Test
+    public void testParser() throws Exception {
+        Parser parser = new Parser();
+        ArrayList<Question> questions = parser.makeQuestions("testDialogs.txt");
+        assertEquals(questions.get(0).question, "hello");
+        assertEquals(questions.get(0).answers.get(0), "hello");
+        assertEquals(questions.get(1).question, "2+2=?");
+        assertEquals(questions.get(1).answers.get(0), "4");
     }
 
 }
